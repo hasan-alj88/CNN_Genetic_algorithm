18014bc5e03daedecd9a7646a2aa662986f439c0
hhj <hasan.alj88@yahoo.com>
2021-03-29 18:11:04 +0300

    Implemenation of CNN __init__ add_change_log change_epoch change_batch_size change_ann_layer_size

--- CNN.py
+++ CNN.py
@@ -1,9 +1,12 @@
+import datetime
+
 import tensorflow as tf
 from keras import layers
+import numpy as np
 import typing
 
 
-def load_data():
+def load__data():
     return tf.keras.datasets.cifar10.load_data()
 
 
@@ -12,17 +15,23 @@ class CNN:
     Creating and Train CNN neural network with defined hyper-parameters
     """
 
-    def __init__(self, hyper_parameters: typing.Dict, Load_data=load_data):
-        (self.x_train, self.y_train), (self.x_test, self.y_test) = Load_data()
+    def __init__(self, hyper_parameters: typing.Dict, load_data=load__data, verbose=0):
+        (self.x_train, self.y_train), (self.x_test, self.y_test) = load_data()
         self.hyper_parameters = hyper_parameters
         self.model = tf.keras.Sequential()
         self.model.add(tf.keras.layers.Input(self.x_train[0].shape))
 
+        # Preprocessing Layers
+        max_val = np.max(self.x_train.flatten)
+        self.model.add(tf.keras.layers.experimental.preprocessing.Rescaling(scale=1.0 / max_val))
+        self.model.add(tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"))
+        self.model.add(tf.keras.layers.experimental.preprocessing.RandomRotation(0.2))
+
         # convolution layers (Feature extraction)
-        cov_layers = self.hyper_parameters['Convolution_layer']
-        filters = self.hyper_parameters['Pool_layers_filter']
-        pool_layers = self.hyper_parameters['Pool_layers_units']
-        strides = self.hyper_parameters['Pool_layers_strides']
+        cov_layers = self.hyper_parameters['convolution_layer']
+        filters = self.hyper_parameters['pool_layers_filter']
+        pool_layers = self.hyper_parameters['pool_layers_units']
+        strides = self.hyper_parameters['pool_layers_strides']
         for layer, (cov, filt, pool, stride) in enumerate(zip(cov_layers, filters, pool_layers, strides)):
             if layer == 0:
                 self.model.add(tf.keras.layers.Conv2D(cov, (filt, filt), strides=stride, activation='relu',
@@ -31,12 +40,100 @@ class CNN:
                 self.model.add(tf.keras.layers.Conv2D(cov, (filt, filt), strides=stride, activation='relu'))
             self.model.add(tf.keras.layers.MaxPool2D((pool, pool)))
         # Fully connected layers
-        for ann_layer, dp_layer in zip(self.hyper_parameters['Fully_connected_layers'],
-                                       self.hyper_parameters['Drop_layers']):
+        for ann_layer, dp_layer in zip(self.hyper_parameters['fully_connected_layers'],
+                                       self.hyper_parameters['drop_layers']):
             self.model.add(tf.keras.layers.Dense(ann_layer, activation='relu'))
             if dp_layer > 0:
                 self.model.add(tf.keras.layers.Dropout(dp_layer))
+        # Output layer
+        self.model.add(tf.keras.layers.Dense(self.output_size, activation='relu'))
+
+        # Compile and Training Model
+        self.model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'])
+        start_time = datetime.datetime.now()
+        self.History = self.model.fit(self.x_train,
+                                      self.y_train,
+                                      epochs=self.hyper_parameters['epochs'],
+                                      batch_size=self.hyper_parameters['batch_size'],
+                                      validation_data=(self.x_test, self.y_train),
+                                      verbose=verbose)
+        self.Training_time = (datetime.datetime.now() - start_time).total_seconds() * 1000
 
     @property
     def input_shape(self):
+        """
+        The CNN Model input shape
+        :return: The CNN Model input shape
+        """
         return self.x_train[0].shape
+
+    @property
+    def output_size(self):
+        """
+
+        :return:
+        """
+        return len(np.unique(np.array(self.y_train).flatten()))
+
+    @staticmethod
+    def add_change_log(hp_dict, change_log):
+        """
+        Logging then changes that have been tuned to arrived to this set of hyper_parameters
+        :param hp_dict: the hyper_parameters dictionary that hold the hyper_parameters set
+        :param change_log: the change log text
+        :return: The hyper_parameters dictionary with the change log updated
+        """
+        if 'changes' not in hp_dict.keys():
+            hp_dict['changes'] = change_log + '\n'
+        else:
+            hp_dict['changes'] += change_log + '\n'
+        return hp_dict
+
+    # Model Changes
+    def change_epoch(self, change):
+        """
+        Increase or decrease the number of epochs
+        :param change: Amount of Epochs to increase (positive int)or decrease (negative int)
+        :return: The hyper_parameters dictionary with Epoch change updated
+        """
+        hp = self.hyper_parameters
+        hp['epochs'] += np.int(change)
+        hp['epochs'] = hp['epochs'] if hp['epochs'] > 1 else 1
+        hp = self.add_change_log(hp, f'Epochs increased by {change}')
+        return hp
+
+    def change_batch_size(self, change):
+        """
+        Increase or decrease the batch size
+        :param change: The batch size increase (positive int)or decrease (negative int)
+        :return: The hyper_parameters dictionary with batch size change updated
+        """
+        hp = self.hyper_parameters
+        hp['batch_size'] += np.int(change)
+        hp['batch_size'] = hp['batch_size'] if hp['batch_size'] > 1 else 1
+        hp = self.add_change_log(hp, f'Batch size increased by {change}')
+        return hp
+
+    def change_ann_layer_size(self, change):
+        """
+        Increase or decrease a randomly selected fully connected layer size
+        :param change: The layer size increase (positive int)or decrease (negative int)
+        :return: The hyper_parameters dictionary with layer size change updated
+        """
+        hp = self.hyper_parameters
+        num_of_layers = len(hp['fully_connected_layers'])
+        layer_to_change = np.random.randint(num_of_layers)
+        layer_size = hp['fully_connected_layers'][layer_to_change]
+        layer_size += np.int(change)
+        layer_size = layer_size if layer_size > self.output_size else self.output_size
+        hp['fully_connected_layers'][layer_to_change] = layer_size
+        hp = self.add_change_log(hp, f'The {th(layer_to_change)} Fully connected layer size increased by {change}')
+        return hp
+
+
+def th(num: int) -> str:
+    the_th = {1: 'st', 2: 'nd', 3: 'rd'}
+    return f'{num}{the_th.get(num, "th")}'
